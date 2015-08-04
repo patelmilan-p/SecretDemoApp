efe5ec86d16451dab76413ef58062ff47cfc0656
Milan Patel <milanpatel@Milans-MacBook-Pro.local>
2015-08-04 20:43:18 +0530

    Added a button to generate secret codes to index view

--- app/controllers/secret_codes_controller.rb
+++ app/controllers/secret_codes_controller.rb
@@ -4,5 +4,7 @@ class SecretCodesController < ApplicationController
   end
 
   def create
+    SecretCode.generate_secret_codes(params[:count])
+    redirect_to secret_codes_path
   end
 end
--- app/models/secret_code.rb
+++ app/models/secret_code.rb
@@ -2,4 +2,8 @@ class SecretCode < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :code
+
+  def generate_secret_codes(count)
+    pass
+  end
 end
