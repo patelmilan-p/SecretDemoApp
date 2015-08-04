400e07923bb74cb0011defebdd781b12a88c3263
Milan Patel <milanpatel@Milans-MacBook-Pro.local>
2015-08-04 18:53:20 +0530

    created admin user from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+User.create(first: 'admin', email: 'admin@secretdemo.com', password: 'secret')
